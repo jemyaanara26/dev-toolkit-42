f787145c157f88ded9bc2b9c7f7f950143edfdb5
Jemya Anara <jemyaypganara@hotmail.com>
2026-09-10 09:06:11 +0200

    add type annotations and docstrings for utils

--- utils.py
+++ utils.py
@@ -1,40 +1,42 @@
+from typing import Any, Callable, Dict, List, TypeVar, Union
 import functools
-import time
-import collections
 
-class MemoizedLRU:
-    def __init__(self, maxsize=128):
-        self.cache = collections.OrderedDict()
-        self.maxsize = maxsize
+T = TypeVar('T')
 
-    def __call__(self, func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            key = (args, tuple(sorted(kwargs.items())))
-            if key in self.cache:
-                self.cache.move_to_end(key)
-                return self.cache[key]
-            result = func(*args, **kwargs)
-            self.cache[key] = result
-            self.cache.move_to_end(key)
-            if len(self.cache) > self.maxsize:
-                self.cache.popitem(last=False)
-            return result
-        return wrapper
+def compose(*functions: Callable[[Any], Any]) -> Callable[[Any], Any]:
+    """chaining of callables into a single execution pipe"""
+    return functools.reduce(lambda f, g: lambda x: g(f(x)), functions)
 
-def batch_process(data, chunk_size=1000):
-    it = iter(data)
-    return iter(lambda: list(itertools.islice(it, chunk_size)), [])
-
-import itertools
-
-def fast_flatten(nested_list):
-    return list(itertools.chain.from_iterable(nested_list))
-
-def profile_execution(func):
+def memoize_with_expiry(func: Callable[..., T]) -> Callable[..., T]:
+    """decorator for caching with an ephemeral memory structure"""
+    cache: Dict[str, T] = {}
+    
     @functools.wraps(func)
-    def timed(*args, **kwargs):
-        start = time.perf_counter()
-        result = func(*args, **kwargs)
-        return result, time.perf_counter() - start
-    return timed
+    def wrapper(*args: Any, **kwargs: Any) -> T:
+        key: str = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+        return cache[key]
+    return wrapper
+
+def deep_flatten(items: List[Any]) -> List[Any]:
+    """recursive flattening of nested list structures"""
+    result: List[Any] = []
+    for item in items:
+        if isinstance(item, list):
+            result.extend(deep_flatten(item))
+        else:
+            result.append(item)
+    return result
+
+def extract_by_key(data: Dict[str, Any], keys: List[str]) -> Dict[str, Any]:
+    """selective dictionary filtering using key mapping"""
+    return {k: v for k, v in data.items() if k in keys}
+
+class Pipeline:
+    """functional pipeline execution engine"""
+    def __init__(self, *funcs: Callable[[Any], Any]) -> None:
+        self.pipeline: Callable[[Any], Any] = compose(*funcs)
+
+    def run(self, value: Any) -> Any:
+        return self.pipeline(value)
